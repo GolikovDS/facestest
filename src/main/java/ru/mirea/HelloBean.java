a55f5ae2c1184800074f1e79330950a9540e73e3
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2015-07-06 13:26:01 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/HelloBean.java
@@ -0,0 +1,21 @@
+package ru.mirea;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
